cce819fe4b9f0aae5feb6abefbefcc752f551c86
ricardojnr <44331283+Ricardojnr@users.noreply.github.com>
2020-05-18 03:07:41 -0400

    d

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -21,9 +21,9 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
-import _ from "lodash";
 
 export default {
   name: "BookList",
