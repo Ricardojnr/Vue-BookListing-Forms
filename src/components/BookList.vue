b967b58343b0c8d8062340fed217cc3e6c4c1ef8
ricardojnr <44331283+Ricardojnr@users.noreply.github.com>
2020-05-18 03:18:17 -0400

    x

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,7 +3,7 @@
     <h1>{{title}}</h1>
     <input v-model="searchInput" type="text" placeholder="Search Books">
     <ul>
-      <book-item v-for='books in searchedBooks' :key='books.id' :book='books'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr><h2> Filtered Books By Ownership</h2>
     <select v-model="holding">
